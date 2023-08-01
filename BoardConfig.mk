b43d812af9a28c5f8257642b5b7532520d84f5f1
kattya9512 <kartiklashkare@gmail.com>
2023-08-01 21:59:57 +0530

    redwood: set BUILD_BROKEN_DUP_RULES := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/redwood
 
+# Ignore overriding commands errors
+BUILD_BROKEN_DUP_RULES := true
+
 # Inherit from sm7325-common
 include device/xiaomi/sm7325-common/BoardConfigCommon.mk
 
