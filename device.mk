ecfe06b7cc105c753df8366a963cc76605bd08b8
nishant6342 <www.rajsonu13@gmail.com>
2023-10-25 22:04:43 +0530

    redwood: Allow updatable apex again - Needed on Android 14

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from sm7325-common
 $(call inherit-product, device/xiaomi/sm7325-common/common.mk)
 
